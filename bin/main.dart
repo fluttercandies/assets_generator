2fe85c5c6a7c7ec593d6b64af327cc820a89742d
zmtzawqlp <zmtzawqlp@live.com>
2019-08-11 22:45:49 +0800

    update for arguments

--- bin/main.dart
+++ bin/main.dart
@@ -2,18 +2,28 @@ import 'package:assets_generator/assets_generator.dart';
 import 'package:build_runner_core/build_runner_core.dart';
 
 main(List<String> arguments) {
-  var packageGraph = arguments.isEmpty
-      ? PackageGraph.forThisPackage()
-      : PackageGraph.forPath(arguments.first);
+  PackageGraph packageGraph;
+  var path =
+      arguments.firstWhere((x) => x.contains("path="), orElse: () => null);
+  if (path != null) {
+    path = path.replaceAll("path=", "");
+    packageGraph = PackageGraph.forPath(path);
+  } else {
+    packageGraph = PackageGraph.forThisPackage();
+  }
 
-  var type = arguments.length == 2
-      ? GenerateType.values[int.parse(arguments[1])]
-      : GenerateType.directory;
+  var type =
+      arguments.firstWhere((x) => x.contains("type="), orElse: () => null);
+  GenerateType generateType = GenerateType.directory;
+  if (type != null) {
+    generateType = GenerateType.values[int.parse(type.replaceAll("type=", ""))];
+  }
 
   if (packageGraph != null) {
     var root = packageGraph.root;
     for (var item in packageGraph.allPackages.values) {
-      generateAssets(packageGraph: item, rootPath: root.path, type: type);
+      generateAssets(
+          packageGraph: item, rootPath: root.path, type: generateType);
     }
   }
 }
